3e30a3b5f84aa960dceb22e601f0964f12067043
Parsa Khodadadi <parsakhodadadi2003@gmail.com>
2018-09-23 09:29:11 -0400

    Revert

--- GitTotourial/ViewController.swift
+++ GitTotourial/ViewController.swift
@@ -11,8 +11,6 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
-        print("Hello Realtyna")
-        print("hello world")
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
